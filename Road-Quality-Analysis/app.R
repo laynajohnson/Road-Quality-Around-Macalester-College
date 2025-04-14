2351d41aebb671e11d8568425f2f724e8766e6f6
laynajohnson <117413709+laynajohnson@users.noreply.github.com>
2025-04-14 12:53:44 -0500

    shiny

--- Road-Quality-Analysis/app.R
+++ Road-Quality-Analysis/app.R
@@ -2,46 +2,99 @@
 library(shiny)
 library(shinythemes)
 library(shinyWidgets)
+library(bslib)
 
 library(tidyverse)
 library(ggplot2)
 library(plotly)
 library(sf)
 library(leaflet)
-library(viridis)
+# library(viridis)
 
+# creating custom theme for shiny app
+custom_theme <- bs_theme(
+  version = 5,
+  bootswatch = "united", # bootswatch theme
+  bg = "#edebeb",        # background
+  fg = "#1e1e1e",        # text
+  primary = "#803025",    
+  base_font = font_google("Ubuntu"),
+  heading_font = font_google("Merriweather"),
+  code_font = font_google("Fira Code")
+)
 
+custom_theme <- bs_add_rules(custom_theme, "
+  .navbar {
+    background-color: #803025 !important;
+  }
+  .navbar .navbar-brand,
+  .navbar-nav > li > a {
+    color: #f3ede6 !important;
+  }
+")
 
 # Define UIs for application 
-ui <- navbarPage(
-
-  title = h1("An Analysis of Road Quality Around Macalester College", style = 'color: #f3ede6; font-size: 40px; margin-top: -10px'),
-  id = "tabs",
-  position = "static",
-  fluid = TRUE,
-  tags$head(
-    tags$style(HTML('.navbar-nav > li > a, .navbar-brand {
-                    height: 80px;
-                    padding-top: 30px
-                    }'))
+ui <- fluidPage(
+  theme = custom_theme,  
+  # creating the title
+  div(
+    style = "background-color: #803025; padding: 20px;",
+    h1("An Analysis of Road Quality Around Macalester College",
+       style = "color: #f3ede6; font-size: 40px; margin: 0;")
   ),
-  theme = shinytheme("yeti"),
   
-  tabPanel("The Project",
-           mainPanel(
-             h1("Introduction"),
-             h5("test")
-           )),
-  tabPanel("Test",
-           sidebarLayout(
-             sidebarPanel(
-               width = 4,
-               h2("some text")
-             ),
+  navbarPage(
+    title = NULL,
+    id = "tabs",
+    position = "static",
+    fluid = TRUE,
+    
+    tags$head(
+      tags$style(HTML('.navbar-nav > li > a, .navbar-brand {
+                      height: 20px;
+                      padding-top: 0px;
+                      }'))
+    ),
+    
+    tabPanel("The Project",
              mainPanel(
-               fluidRow()
-             )
-           ))
+               h1("Introduction"),
+               h5("test")
+             )),
+    
+    tabPanel("The Data",
+             sidebarLayout(
+               sidebarPanel(
+                 width = 4,
+                 h2("some text")
+               ),
+               mainPanel(
+                 fluidRow()
+               )
+             )),
+    
+    tabPanel("Analysis and Exploration",
+             sidebarLayout(
+               sidebarPanel(
+                 width = 4,
+                 h2("some text")
+               ),
+               mainPanel(
+                 fluidRow()
+               )
+             )),
+    
+    tabPanel("Implications",
+             sidebarLayout(
+               sidebarPanel(
+                 width = 4,
+                 h2("some text")
+               ),
+               mainPanel(
+                 fluidRow()
+               )
+             ))
+  )
 )
 
 # Define server logic required to draw a histogram
